6d73b9eacca8bd0ae8b6f3666d76e8505e52e1de
PiotrSzymczyk <ja.piotrek.szymczyk@gmail.com>
2016-03-18 16:57:10 +0100

    Tested & working

--- app/src/main/java/whatever/gamepool/Logic2048.java
+++ app/src/main/java/whatever/gamepool/Logic2048.java
@@ -38,15 +38,16 @@ public class Logic2048 {
         boolean valueIs2 = rand.nextDouble() < 0.9;
         // Find n'th empty position, if reached end of board, start over
         while( n > 0 ){
-            if( board[x][y] == 0 ) n--; // Counting only empty fields
             y++;
-            if( y == SIZE - 1 ){
+            if( y == SIZE ){
                 x++;
                 y = 0;
             }
             if( x == SIZE ){
                 x = 0;
             }
+            if( board[x][y] == 0 ) n--; // Counting only empty fields
+
         }
         // Set selected field using appropriate value
         if ( valueIs2 ){
@@ -57,20 +58,19 @@ public class Logic2048 {
     }
 
     // To be replaced by event listener
-    public void moveAndFill(int direction){
+    public void moveAndFill(Direction d){
         // direction described by number position on clock board, for example 12 for up, 9 for left
-        direction = direction % 12;
-        switch(direction){
-            case 0:
+        switch(d){
+            case UP:
                 moveUp();
                 break;
-            case 3:
+            case RIGHT:
                 moveRight();
                 break;
-            case 6:
+            case DOWN:
                 moveDown();
                 break;
-            case 9:
+            case LEFT:
                 moveLeft();
                 break;
             default:
@@ -84,54 +84,65 @@ public class Logic2048 {
         }
         fillRandom();
     }
-
-    private void moveRight() {
+    private boolean moveRight() {
+        boolean changed = false;
         for ( int i = 0; i < SIZE; i++ ){  // for each row
             int positionToWrite = SIZE -1;          // position for join
             int cache = board [i][positionToWrite]; // value for join
 
             for( int j =  SIZE - 2; j >= 0; j-- ){
                 if( (board[i][j] == cache) & (cache != 0) ){
+                    changed = true;
                     score += 2 * cache;
                     board[i][positionToWrite] = 2 * cache;
                     positionToWrite--;
                     cache = 0;
                 } else if ( (cache == 0) & (board[i][j] != 0) ) {
                     cache = board[i][j];
-                } else if ( board [i][j] != cache ){
+                } else if ( board [i][j] != cache  & (board[i][j] != 0)  ){
+                    changed = true;
+                    board[i][positionToWrite] = cache;
                     cache = board [i][j];
                     positionToWrite--;
                 }
             }
             if ( cache != 0 ){
+                changed = true;
                 board[i][positionToWrite] = cache;
                 positionToWrite--;
             }
-            while ( positionToWrite > 0 ){
+            while ( positionToWrite >= 0 ){
                 board[i][positionToWrite] = 0;
                 positionToWrite--;
             }
         }
+        return changed;
     }
-    private void moveLeft() {
+
+    private boolean moveLeft() {
+        boolean changed = false;
         for ( int i = 0; i < SIZE; i++ ){  // for each row
             int positionToWrite = 0;                // position for join
             int cache = board [i][positionToWrite]; // value for join
 
             for( int j =  1; j < SIZE; j++ ){
                 if( (board[i][j] == cache) & (cache != 0) ){
+                    changed = true;
                     score += 2 * cache;
                     board[i][positionToWrite] = 2 * cache;
                     positionToWrite++;
                     cache = 0;
                 } else if ( (cache == 0) & (board[i][j] != 0) ) {
                     cache = board[i][j];
-                } else if ( board [i][j] != cache ){
+                } else if ( board [i][j] != cache  & (board[i][j] != 0) ){
+                    changed = true;
+                    board[i][positionToWrite] = cache;
                     cache = board [i][j];
                     positionToWrite++;
                 }
             }
             if ( cache != 0 ){
+                changed = true;
                 board[i][positionToWrite] = cache;
                 positionToWrite++;
             }
@@ -140,8 +151,47 @@ public class Logic2048 {
                 positionToWrite++;
             }
         }
+        return changed;
     }
-    private void moveDown() {
+
+
+    private boolean moveUp() {
+        boolean changed = false;
+        for ( int i = 0; i < SIZE; i++ ){  // for each column
+            int positionToWrite = 0;                // position for join
+            int cache = board[positionToWrite][i];  // value for join
+
+            for( int j =  1; j < SIZE; j++ ){
+                if( (board[j][i] == cache) & (cache != 0) ){
+                    changed = true;
+                    score += 2 * cache;
+                    board[positionToWrite][i] = 2 * cache;
+                    positionToWrite++;
+                    cache = 0;
+                } else if ( (cache == 0) & (board[j][i] != 0) ) {
+                    cache = board[j][i];
+                } else if ( board[j][i] != cache  & (board[j][i] != 0) ){
+                    changed = true;
+                    board [positionToWrite][i] = cache;
+                    cache = board[j][i];
+                    positionToWrite++;
+                }
+            }
+            if ( cache != 0 ){
+                changed = true;
+                board[positionToWrite][i] = cache;
+                positionToWrite++;
+            }
+            while ( positionToWrite < SIZE ){
+                board[positionToWrite][i] = 0;
+                positionToWrite++;
+            }
+        }
+        return changed;
+    }
+
+    private boolean moveDown() {
+        boolean changed = false;
         for ( int i = 0; i < SIZE; i++ ){  // for each column
             int positionToWrite = SIZE - 1;         // position for join
             int cache = board [positionToWrite] [i];// value for join
@@ -149,52 +199,66 @@ public class Logic2048 {
             for( int j =  SIZE - 2; j >= 0; j-- ){
                 score += 2 * cache;
                 if( (board[j][i] == cache) & (cache != 0) ){
+                    changed = true;
                     board[positionToWrite][i] = 2 * cache;
                     positionToWrite--;
                     cache = 0;
                 } else if ( (cache == 0) & (board[j][i] != 0) ) {
                     cache = board[j][i];
-                } else if ( board[j][i] != cache ){
+                } else if ( board[j][i] != cache  & (board[j][i] != 0) ){
+                    changed = true;
+                    board [positionToWrite][i] = cache;
                     cache = board[j][i];
                     positionToWrite--;
                 }
             }
             if ( cache != 0 ){
+                changed = true;
                 board[positionToWrite][i] = cache;
                 positionToWrite--;
             }
-            while ( positionToWrite > 0 ){
+            while ( positionToWrite >= 0 ){
                 board[positionToWrite][i] = 0;
                 positionToWrite--;
             }
         }
+        return changed;
     }
-    private void moveUp() {
-        for ( int i = 0; i < SIZE; i++ ){  // for each column
-            int positionToWrite = 0;                // position for join
-            int cache = board[positionToWrite][i];  // value for join
 
-            for( int j =  1; j < SIZE; j++ ){
-                if( (board[j][i] == cache) & (cache != 0) ){
-                    score += 2 * cache;
-                    board[positionToWrite][i] = 2 * cache;
-                    positionToWrite++;
-                    cache = 0;
-                } else if ( (cache == 0) & (board[j][i] != 0) ) {
-                    cache = board[j][i];
-                } else if ( board [j][i] != cache ){
-                    cache = board [j][i];
-                    positionToWrite++;
-                }
+    public void print(){
+        for(int i = 0; i < SIZE; i++){
+            for (int j = 0; j < SIZE; j++){
+                System.out.print(board[i][j] + " ");
             }
-            if ( cache != 0 ){
-                board[positionToWrite][i] = cache;
-                positionToWrite++;
-            }
-            while ( positionToWrite < SIZE ){
-                board[positionToWrite][i] = 0;
-                positionToWrite++;
-            }
+            System.out.println();
         }
+        System.out.println();
+    }
+
+    public static void main(String [] args){
+        Logic2048 test = new Logic2048();
+        test.print();
+        System.out.println("---START---\n");
+        test.moveAndFill(Direction.UP);
+        test.print();
+        test.moveAndFill(Direction.UP);
+        test.print();
+        test.moveAndFill(Direction.RIGHT);
+        test.print();
+        test.moveAndFill(Direction.UP);
+        test.print();
+        test.moveAndFill(Direction.UP);
+        test.print();
+        test.moveAndFill(Direction.LEFT);
+        test.print();
+        test.moveAndFill(Direction.UP);
+        test.print();
+        test.moveAndFill(Direction.DOWN);
+        test.print();
+        test.moveAndFill(Direction.UP);
+        test.print();
+    }
+    public enum Direction{
+        UP, RIGHT, DOWN, LEFT
     }
 }
