585368a19f3954ffd8bdabbc71ae119e60f4ca52
PiotrSzymczyk <ja.piotrek.szymczyk@gmail.com>
2016-06-08 23:28:28 +0200

    15 Added to menu

--- app/src/main/java/whatever/gamepool/ItemContainer.java
+++ app/src/main/java/whatever/gamepool/ItemContainer.java
@@ -1,13 +1,17 @@
 package whatever.gamepool;
 
+import whatever.Fifteen.Game15Activity;
+import whatever.Fifteen.Menu15Activity;
 import whatever.game2048.Menu2048Activity;
 import whatever.gameRace.MenuRaceActivity;
 
 public class ItemContainer {
 
-    private final Class[] activityClasses = {Menu2048Activity.class, MenuRaceActivity.class};
-    private final String[][] activityDescriptions = {{"2048", "An easy game, where we have to merge numbers"}, {"Race", "An action game, where you drive a car and avoid collisions with others"}};
-    private final int[] activityIcons = {R.drawable.item_2048, R.drawable.item_race};
+    private final Class[] activityClasses = {Menu2048Activity.class, MenuRaceActivity.class, Menu15Activity.class};
+    private final String[][] activityDescriptions = {{"2048", "An easy game, where we have to merge numbers"},
+            {"Race", "An action game, where you drive a car and avoid collisions with others"},
+            {"Fifteen", "Simple logic puzzle which aim is to put elements in correct order"}};
+    private final int[] activityIcons = {R.drawable.item_2048, R.drawable.item_race, R.drawable.item_race};
 
     private static ItemContainer ourInstance = new ItemContainer();
 
